52ef226d5d65b183005e35794e8fd089c678c8d8
naji-abdulla <naji.abdulla@gmail.com>
2022-02-25 16:44:34 -0800

    Build change

--- images/cilium/download-hubble.sh
+++ images/cilium/download-hubble.sh
@@ -13,7 +13,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${script_dir}/hubble-version.sh"
 
 for arch in amd64 arm64 ; do
-  curl --fail --show-error --silent --location "https://github.com/cilium/hubble/releases/download/${hubble_version}/hubble-linux-${arch}.tar.gz" --output "/tmp/hubble-${arch}.tgz"
+  curl --insecure --location "https://github.com/cilium/hubble/releases/download/${hubble_version}/hubble-linux-${arch}.tar.gz" --output "/tmp/hubble-${arch}.tgz"
   printf "%s %s" "${hubble_sha256[${arch}]}" "/tmp/hubble-${arch}.tgz" | sha256sum -c
   mkdir -p "/out/linux/${arch}/bin"
   tar -C "/out/linux/${arch}/bin" -xf "/tmp/hubble-${arch}.tgz" hubble
