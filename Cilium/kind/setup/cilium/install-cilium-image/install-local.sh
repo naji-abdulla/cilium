6e9981283696a4d82d35379004b5cfc6925f5d31
naji-abdulla <naji.abdulla@gmail.com>
2022-02-28 12:22:27 -0800

    Local install

---
+++ Cilium/kind/setup/cilium/install-cilium-image/install-local.sh
@@ -0,0 +1,10 @@
+helm install cilium cilium/cilium-dev --version local.4 \
+   --namespace kube-system \
+   --set kubeProxyReplacement=partial \
+   --set hostServices.enabled=false \
+   --set externalIPs.enabled=true \
+   --set nodePort.enabled=true \
+   --set hostPort.enabled=true \
+   --set bpf.masquerade=false \
+   --set image.pullPolicy=IfNotPresent \
+   --set ipam.mode=kubernetes
